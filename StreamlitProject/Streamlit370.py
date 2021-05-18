d7e40878ac2f37098a4d10e2e92d2f6fdfcbe44b
motayech <79416174+motayech@users.noreply.github.com>
2021-05-19 01:03:12 +0400

    Update Streamlit370.py

--- StreamlitProject/Streamlit370.py
+++ StreamlitProject/Streamlit370.py
@@ -16,7 +16,7 @@ from sklearn.tree import DecisionTreeClassifier
 from sklearn.ensemble import RandomForestClassifier
 
 st.set_option('deprecation.showPyplotGlobalUse', False)
-st.set_page_config(page_title = 'Store X Analysis Tool')
+st.set_page_config(page_title = 'Store X Analysis Tool', layout = 'wide')
 
 dataset = st.file_uploader("Please upload your dataset of interest here!", type = ["csv"])
 
